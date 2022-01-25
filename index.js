5b6b612bebb9aa1b30fd0da26299f092b8b8ba78
Bogdan-secret <boggrachev22005@gmail.com>
2022-01-25 16:44:03 +0200

    jg

---
+++ index.js
@@ -0,0 +1,36 @@
+
+// Площадь треугольника
+
+// const length = Number(prompt('Введите длину прямоугольника'));
+// const width = Number(prompt('Введите ширину прямоугольника'));
+// console.log (length * width);
+
+
+// Площадь трапеции
+
+// const minLength = Number(prompt('Введите меньшее основание трапеции'));
+// const maxLength = Number(prompt('Введите большее основание трапеции'));
+// const h = Number(prompt('Введите высоту трапеции'));
+// console.log((minLength+maxLength) / 2 * h);
+
+// Площадь круга
+
+// const r = Number(prompt('Введите радиус круга'));
+// console.log (Math.PI * r * r);
+
+// Площадь параллелограмма
+
+// const a = Number(prompt('Введите основание параллелограмма'));
+// const h = Number(prompt('Введите высоту параллелограмма'));
+// console.log(a * h);
+
+//Объем цилиндра
+
+// const r = Number(prompt('Введите радиус цилиндра'));
+// const h = Number(prompt('Введите высоту цилиндра'));
+// console.log(Math.PI * r * r * h);
+
+
+//Объем сферы
+// const r = Number(prompt('Введите радиус сферы'));
+// console.log(4/3 * Math.PI * r * r * r);
